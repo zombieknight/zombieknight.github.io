91daa7f54184009bff4e3e674bc420a135976db6
zombieknight <43140203+zombieknight@users.noreply.github.com>
2018-10-08 10:10:24 -0500

    updated info for using Json

--- json/js/json_sketch.js
+++ json/js/json_sketch.js
@@ -1,17 +1,34 @@
-var me;
-
-
+var birdData;
+var allBirds;
 function preload(){
-	me = loadJSON("me.json");
+birdData = loadJSON("./js/bird.json");
 
-}
+} 
 
 function setup(){
-print(me);
+	noCanvas();
+	print(birdData);
 
+	allBirds = birdData.birds;
+	print(allBirds.length);
+	for(var i =0; i < allBirds.length; i++){
+		createElement('h1', allBirds[i].family);
+
+		var members = allBirds[i].members;
+		for(var j =0; j < members.length; j++){
+		createElement('h1', members[j]);
+	}
+	}
+	
 }
 
 function draw(){
-
+background(0);
+fill(255);
+textSize(60);
+text(birdData.birds[2].family,20,40);
+textSize(40);
+text(birdData.birds[2].members[2],20,80);
+text(birdData.birds[2].members[3],20,120);
 
 }
